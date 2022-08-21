9dfb0f3ffca2c0738c3eac8a39f9d7a241abadbd
mehdi <mkoushki.79@gmail.com>
2022-08-21 04:56:15 -0700

    add a page hashtag list

--- src/components/pages/tweetByHashtag/Hashtag.js
+++ src/components/pages/tweetByHashtag/Hashtag.js
@@ -1,9 +1,29 @@
+import { Tag } from '@mui/icons-material'
 import React from 'react'
 import styles from './hashtag.module.scss'
+import profil from '../../../images/Profile1.jpg'
+import TiweeetList from '../../tiweetList/TweetList'
+
 
 const Hashtag = () => {
   return (
-    <div>Hashtag</div>
+
+    <div className={styles.Main}>
+
+        <div className={styles.Topbar}>
+          
+        <div className={styles.ConetentItem}>
+        <Tag className={styles.tag}/>
+        <span>کرونا ویروس</span>
+      </div>      
+
+        </div>
+
+       
+        
+            <TiweeetList/>
+      
+    </div>
   )
 }
 
